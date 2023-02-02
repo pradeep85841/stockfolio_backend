6e4f97f829ab2964a79b39e12c0d1910ec6a534e
pradeep85841 <97498198+pradeep85841@users.noreply.github.com>
2023-02-02 15:58:06 +0530

    ENV ADDED

--- mongoDbOperations/mongoConnect.js
+++ mongoDbOperations/mongoConnect.js
@@ -1,11 +1,11 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-
+const DB = process.env.MONGO_DATABASE
 const MongoClient = require('mongodb').MongoClient;
 
- var uri = "mongodb+srv://developer:root@stockfolio.rnte1rd.mongodb.net/?retryWrites=true&w=majority";
+// var uri = "mongodb+srv://developer:root@stockfolio.rnte1rd.mongodb.net/?retryWrites=true&w=majority";
 //var uri = 'mongodb://localhost:27017'
-const client = new MongoClient(uri)
+const client = new MongoClient(DB)
 
 const connection = client.connect((err)=>{
 if(err) throw err;
