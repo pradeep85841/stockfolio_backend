fe87cfd16dff1785bd141c71ef24ce4ac59d6778
pradeep85841 <97498198+pradeep85841@users.noreply.github.com>
2023-02-03 18:18:56 +0530

    res.header removed

--- server.js
+++ server.js
@@ -21,11 +21,12 @@ const port = process.env.PORT || 5000;
   })
 );*/
 
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+*/
 
 import db from "./mongoDbOperations/mongoConnect.js";
 const client = db;
