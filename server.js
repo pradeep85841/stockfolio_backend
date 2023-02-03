f17c9a3f6536346211b1417db0668a1b10043bf3
pradeep85841 <97498198+pradeep85841@users.noreply.github.com>
2023-02-03 18:04:02 +0530

    header method removed

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ const port = process.env.PORT || 5000;
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
